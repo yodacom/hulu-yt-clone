097e606bd111eaca6dde8e2c0b26c0eb1cf2ca41
Jeremy Black <jb@yodacom.com>
2021-10-22 13:51:52 -0600

    final commit 2

--- components/Nav.js
+++ components/Nav.js
@@ -13,7 +13,7 @@ function Nav() {
           <h2
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
-            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 "
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
           </h2>
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -22,8 +22,8 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {result.media_type && `${result.media_type} * `}{" "}
-          {result.release_date || result.first_air_date} * {" "}
+          {result.media_type && `${result.media_type} •`}{" "}
+          {result.release_date || result.first_air_date} •{" "}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
